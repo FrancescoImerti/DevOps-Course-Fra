c921235da6a53ced170635f4d4e05484d0fbb93f
FrancescoImerti <fra.imerti@gmail.com>
2023-08-06 19:26:14 +0100

    first exercise completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,4 +8,14 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=get_items())
+
+@app.route('/add-item', methods=['POST'])
+def add_new_item():    
+    add_item(request.form.get('field_name'))
+    return redirect('/')
+
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, threaded=True)
