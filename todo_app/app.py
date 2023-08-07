95202d540674913be07aeb2034978edd3b506f24
FrancescoImerti <fra.imerti@gmail.com>
2023-08-07 01:21:03 +0100

    Optional tasks completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from todo_app.data.session_items import get_items, add_item
+from todo_app.data.session_items import get_items, add_item, delete_item, get_item, save_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,11 +8,23 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=get_items())
+    return render_template('index.html', items=sorted(get_items(), key= lambda x : x['status']))
 
 @app.route('/add-item', methods=['POST'])
 def add_new_item():    
     add_item(request.form.get('field_name'))
+    return redirect('/')
+
+@app.route('/update-item', methods=['POST'])
+def update_item_status():
+    item = get_item(request.form.get('id_to_edit'))
+    item["status"] = 'Completed'  
+    save_item(item)
+    return redirect('/')
+
+@app.route('/remove-item', methods=['POST'])
+def remove_item():    
+    delete_item(request.form.get('id_to_remove'))
     return redirect('/')
 
 
